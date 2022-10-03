72a5537503f9d3ec5a986755566029f468561539
Harsh Chaurasia <harshchaurasiamgs@gmail.com>
2022-10-03 16:37:04 +0530

    Add project files.

---
+++ AngularDemo1/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularDemo1.Models
+{
+    public class Employee
+    {
+        public int Employeeid { get; set; }
+        public string EmployeeName { get; set; }
+        public string DateOfjoining { get; set; }
+        public string Department { get; set; }
+        public string PhotoFileName { get; set; }
+
+
+
+
+    }
+}
